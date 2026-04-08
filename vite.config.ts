c397fa8c5457f02a8f0b010d9c546c67f8ac4604
ricca1504 <pedrosalvadorpa@hotmail.com>
2026-04-08 14:20:56 +0000

    Add GitHub Actions build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // ← FONDAMENTALE per Electron
+  base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    minify: false,
   }
 });
